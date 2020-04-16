dae70d8ab293c910ba1b72351fe3a4aaa2ff705e
stfurkan <35101659+stfurkan@users.noreply.github.com>
2020-04-15 21:20:26 -0400

    about page updated

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,45 +4,44 @@ import PageTitle from '../layouts/PageTitle';
 
 export default function About({ lang }) {
   return (
-    <div className='ui container'>
+    <section className='section'>
       <PageTitle title={lang.pageTitle} metaContent={lang.metaContent} />
-      <br />
-      <div>
-        <h1 className='ui top attached header'>{lang.pageHeader}</h1>
-        <div className='ui attached segment'>
-          <div className='ui segment raised'>
-            <p>
-              <span className='ui big text'>{lang.aboutText1}</span>
-            </p>
-            <p>
-              <span className='ui big text'>{lang.aboutText2}</span>
-            </p>
-            <p>
-              <span className='ui big text'>
-                <b>{lang.developer}:</b>{' '}
-                <a
-                  href='https://www.linkedin.com/in/s-furkan-teke-50758513b'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Furkan
-                </a>
-              </span>
-            </p>
-            <p>
-              <span className='ui large text'>
-                <a
-                  href='https://github.com/stfurkan'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <i className='github icon'></i> GitHub
-                </a>
-              </span>
-            </p>
-          </div>
+
+      <div className='container'>
+        <div className='box content'>
+          <h1 className='is-size-2'>{lang.pageHeader}</h1>
+
+          <p>
+            <span className='is-size-5'>{lang.aboutText1}</span>
+          </p>
+          <p>
+            <span className='is-size-5'>{lang.aboutText2}</span>
+          </p>
+          <p>
+            <span className='is-size-6'>
+              <b>{lang.developer}:</b>{' '}
+              <a
+                href='https://www.linkedin.com/in/s-furkan-teke-50758513b'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Furkan
+              </a>
+            </span>
+          </p>
+          <p>
+            <span className='is-size-6'>
+              <a
+                href='https://github.com/stfurkan'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <i className='fab fa-github'></i> GitHub
+              </a>
+            </span>
+          </p>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
