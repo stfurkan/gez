2e73a590ca0babc6b77a403823d50438ed75f165
stfurkan <35101659+stfurkan@users.noreply.github.com>
2020-04-30 02:41:28 -0400

    add icon to header buttons

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -49,15 +49,17 @@ export default class Header extends Component {
           <div className={menu ? 'navbar-menu is-active' : 'navbar-menu'}>
             <div className='navbar-start'>
               <Link to='/' className='navbar-item is-hidden-desktop'>
-                {lang.homepage}
+                <i className='fas fa-home'></i> {lang.homepage}
               </Link>
               <Link to='/favorites' className='navbar-item is-hidden-desktop'>
-                {lang.favorites}
+                <i className='fas fa-heart'></i> {lang.favorites}
               </Link>
 
               <div className='buttons navbar-item is-hidden-touch'>
                 <Link to='/' className='button is-primary'>
-                  <strong>{lang.homepage}</strong>
+                  <strong>
+                    <i className='fas fa-home'></i> {lang.homepage}
+                  </strong>
                 </Link>
                 <Link to='/favorites' className='button is-primary'>
                   <strong>
@@ -69,18 +71,22 @@ export default class Header extends Component {
 
             <div className='navbar-end'>
               <Link to='/faq' className='navbar-item is-hidden-desktop'>
-                {lang.faq}
+                <i className='fas fa-question'></i> {lang.faq}
               </Link>
               <Link to='/about' className='navbar-item is-hidden-desktop'>
-                {lang.about}
+                <i className='fas fa-info-circle'></i> {lang.about}
               </Link>
 
               <div className='buttons navbar-item is-hidden-touch'>
                 <Link to='/faq' className='button is-success'>
-                  <strong>{lang.faq}</strong>
+                  <strong>
+                    <i className='fas fa-question'></i> {lang.faq}
+                  </strong>
                 </Link>
                 <Link to='/about' className='button is-info'>
-                  <strong>{lang.about}</strong>
+                  <strong>
+                    <i className='fas fa-info-circle'></i> {lang.about}
+                  </strong>
                 </Link>
               </div>
             </div>
