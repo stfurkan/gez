dd7ae0f1dc25e10d0057c99d0ba0bcce26b76b47
stfurkan <35101659+stfurkan@users.noreply.github.com>
2020-05-23 14:13:47 -0400

    update footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -7,47 +7,80 @@ function Footer({ lang }) {
       <hr />
       <footer className='footer is-paddingless has-background-warning'>
         <div className='content has-text-centered'>
-          <p>
+          <div>
             <Link className='item' to='/'>
-              {lang.homepage}
+              <span className='icon is-medium'>
+                <i className='fas fa-lg fa-home'></i>
+              </span>
             </Link>{' '}
-            |{' '}
             <Link className='item' to='/about'>
-              {lang.about}
+              <span className='icon is-medium'>
+                <i className='fas fa-lg fa-info-circle'></i>
+              </span>
             </Link>{' '}
-            |{' '}
             <a
               className='item'
               href='https://github.com/stfurkan/gez'
               target='_blank'
               rel='noopener noreferrer'
             >
-              <i className='fab fa-github'></i> GitHub
+              <span className='icon is-medium'>
+                <i className='fab fa-lg fa-github'></i>
+              </span>
             </a>{' '}
-            |{' '}
             <a
               className='item'
               href='https://twitter.com/lagezla'
               target='_blank'
               rel='noopener noreferrer'
             >
-              <i className='fab fa-twitter'></i> Twitter
+              <span className='icon is-medium'>
+                <i className='fab fa-lg fa-twitter'></i>
+              </span>
             </a>{' '}
-            |{' '}
             <a
               className='item'
               href='https://www.instagram.com/lagezla'
               target='_blank'
               rel='noopener noreferrer'
             >
-              <i className='fab fa-instagram'></i> Instagram
+              <span className='icon is-medium'>
+                <i className='fab fa-lg fa-instagram'></i>
+              </span>
             </a>
-          </p>
-          <p>{lang.text}</p>
+          </div>
+          <Credit />
+          <div className='has-text-weight-medium'>{lang.text}</div>
         </div>
       </footer>
     </div>
   );
 }
+
+const Credit = () => {
+  return navigator.language === 'tr' || navigator.language === 'tr-TR' ? (
+    <div className='has-text-weight-medium is-size-7'>
+      <a
+        href='https://www.linkedin.com/in/stfurkan'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Furkan Teke
+      </a>{' '}
+      tarafından geliştirilmiştir.
+    </div>
+  ) : (
+    <div className='has-text-weight-medium is-size-7'>
+      Developed by{' '}
+      <a
+        href='https://www.linkedin.com/in/stfurkan'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Furkan Teke
+      </a>
+    </div>
+  );
+};
 
 export default Footer;
