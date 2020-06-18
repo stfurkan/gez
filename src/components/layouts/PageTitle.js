7f4baebc23e815f88b7a06f031703d8ba693e376
stfurkan <35101659+stfurkan@users.noreply.github.com>
2020-06-18 02:04:42 -0400

    update twitter card image

--- src/components/layouts/PageTitle.js
+++ src/components/layouts/PageTitle.js
@@ -7,15 +7,6 @@ export default function PageTitle({ title, metaContent }) {
       <Helmet>
         <title>{title}</title>
         <meta name='description' content={metaContent} />
-
-        <meta name='twitter:card' content='summary' />
-        <meta name='twitter:site' content='@lagezla' />
-        <meta name='twitter:title' content={title} />
-        <meta name='twitter:description' content={metaContent} />
-        <meta
-          name='twitter:image'
-          content='https://gez.la/static/img/logo.png'
-        />
       </Helmet>
     </div>
   );
