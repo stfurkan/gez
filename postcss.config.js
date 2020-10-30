329b63dfd41eeaa22bc113e67e597677669059d3
stfurkan <35101659+stfurkan@users.noreply.github.com>
2020-10-30 15:43:13 -0400

    update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  plugins: ['tailwindcss', 'postcss-preset-env', 'autoprefixer']
+  plugins: [
+    'tailwindcss',
+    'postcss-preset-env',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+          grid: 'autoplace'
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false
+        }
+      }
+    ]
+  ]
 };
