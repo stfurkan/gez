5ff5b83f6928734aa6b6689cc2f1b203d45bb1a2
stfurkan <35101659+stfurkan@users.noreply.github.com>
2020-11-01 04:00:05 -0500

    purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,27 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true
   },
-  purge: [],
+  purge: {
+    content: [
+      './components/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}'
+    ],
+    options: {
+      whitelist: [
+        'my-3',
+        'text-3xl',
+        'font-bold',
+        'border-solid',
+        'border-b-2',
+        'pt-4',
+        'pb-4',
+        'border-gray-600',
+        'list-disc',
+        'pl-10',
+        'pb-2'
+      ]
+    }
+  },
   theme: {
     extend: {}
   },
